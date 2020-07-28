da87383d59b74591d4ec947e3b199d5d26dc6d29
aacebo <44811138+aacebo@users.noreply.github.com>
2020-07-28 14:12:43 -0400

    move files around a bit

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.query);
   res.statusCode = 200;
   res.json({ name: 'John Doe' });
 }
