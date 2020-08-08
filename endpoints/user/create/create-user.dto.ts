121a1df7885aeb6f875bb1ac64be2f88246eb50b
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2020-08-08 13:59:05 -0400

    add custom validation messages

--- endpoints/user/create/create-user.dto.ts
+++ endpoints/user/create/create-user.dto.ts
@@ -1,17 +1,18 @@
-import { IsEmail, Length, MinLength } from 'class-validator';
+import { IsEmail, Length, MinLength, Matches } from 'class-validator';
 
 export default class CreateUser {
-  @MinLength(1)
+  @MinLength(1, { message: 'required' })
   readonly firstName: string;
 
-  @MinLength(1)
+  @MinLength(1, { message: 'required' })
   readonly lastName: string;
 
-  @IsEmail()
-  @MinLength(1)
+  @IsEmail(undefined, { message: 'must be a valid email' })
+  @MinLength(1, { message: 'required' })
   readonly email: string;
 
-  @Length(8, 25)
+  @Length(8, 25, { message: 'must be length 8-25' })
+  @Matches(/^(?=.*\d).$/, { message: 'must have at least 1 number' })
   readonly password: string;
 
   constructor(user?: Partial<CreateUser>) {
