ae1cb02381d8ef217d343b1aca834eb133500183
Alex Acebo <44811138+aacebo@users.noreply.github.com>
2020-08-10 23:20:32 -0400

    Update index.tsx

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import GoogleButton from '../../components/google-button/google-button';
 import isAuthOrRedirect from '../../middleware/is-auth-or-redirect/is-auth-or-redirect.middleware';
 
@@ -6,6 +8,10 @@ import styles from './index.module.scss';
 export default function UserIndex() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Lucid: User</title>
+      </Head>
+
       <GoogleButton />
     </div>
   );
